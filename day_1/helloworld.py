93d52eb34ac5b105e3d484837b818ab436dcac26
bibexaliyy <hisah075@gmail.com>
2024-11-16 08:32:03 +0100

    Initial commit

---
+++ day_1/helloworld.py
@@ -0,0 +1,24 @@
+a=3
+b=4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a%b)
+print(a/b)
+print(a**b)
+print(a//b)
+
+#writing strings
+print("Habiba Isah")
+print("Sheshi Makpa")
+print("Nigeria")
+print("I am enjoying 30 days of python")
+
+#Chek the data types
+print(type(10))
+print(type(9.8))
+print(type(3.4))
+print(type(4-4j))
+print(type([['Asabeneh', 'Python', 'Finland']]))
+print(type("Habiba Isah"))
+print(type("Sheshi Makpa"))
